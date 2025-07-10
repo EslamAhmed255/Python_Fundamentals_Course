93859c806b5855c09bf8fd1dce0068e80fea7944
Eslam Ahmed <eslam.ahmed.mohamad@gmail.com>
2025-07-10 12:56:31 +0300

    Delete Chapter 5 directory

--- Chapter 5/string to list.py
+++
@@ -1,7 +0,0 @@
-# Split = it's the function that responisble to transfer from string to list using COMMA
-
-names_string = input("Enter names separated by a comma \n")
-
-name = names_string.split(", ")
-
-print(name)
